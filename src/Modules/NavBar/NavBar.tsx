b996b9a7680cf92294bf28669e8eee8036fd0205
Paul Hume <paul@paulhume.co.uk>
2023-02-02 17:45:59 +0100

    #12: Add nav icons and mobile theme toggle

--- src/Modules/NavBar/NavBar.tsx
+++ src/Modules/NavBar/NavBar.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import { FilterAlt } from '@mui/icons-material';
-import { Drawer, IconButton } from '@mui/material';
+import { Description,FilterAlt, Home } from '@mui/icons-material';
+import { IconButton } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -11,7 +11,6 @@ import { TagDrawer } from 'Modules/TagDrawer';
 
 import styles from './NavBar.module.css';
 
-import { useUi } from 'Context/uiContext';
 import { useMedia } from 'Hooks';
 
 interface NavBarProps {
@@ -19,7 +18,7 @@ interface NavBarProps {
 }
 
 export const NavBar = ({ location = 'header' }: NavBarProps) => {
-  const { theme, toggleTheme, currentTheme } = useUi();
+  
   const largeScreen = useMedia('md');
   const [open, setOpen] = useState(false);
 
@@ -32,20 +31,22 @@ export const NavBar = ({ location = 'header' }: NavBarProps) => {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar className={`${styles.buttons} ${location === 'footer' ? styles.footer : ''}`}>
-          {largeScreen && location === 'header' && <ThemeToggleSwitch checked={theme === 'dark'} onChange={toggleTheme} theme={currentTheme} />}
+          {largeScreen && location === 'header' && <ThemeToggleSwitch />}
           {!largeScreen && location === 'header' && (
             <>
               <IconButton color="inherit" onClick={() => toggleDrawer(true)}>   
                 <FilterAlt />
               </IconButton>
 
+              <ThemeToggleSwitch />
+
               <TagDrawer open={open} onClose={() => toggleDrawer(false)} />
             </>
           )}
           {((largeScreen && location === 'header') || (!largeScreen && location === 'footer')) && (
             <>
-              <NavLink to="/">Home</NavLink>
-              <NavLink to="/journal">Journal</NavLink>
+              <NavLink to="/" icon={<Home />}>Home</NavLink>
+              <NavLink to="/journal" icon={<Description />}>Journal</NavLink>
               {/* <NavLink to="/projects">Projects</NavLink>
             <NavLink to="/history">History</NavLink> */}
             </>
