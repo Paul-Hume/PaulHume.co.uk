c9f3481a80a8388eb78cf674117171cc2f60dbd7
Paul Hume <paul@paulhume.co.uk>
2023-02-07 11:24:54 +0100

    #18: Remove journal entries from home page

--- src/Routes/Home/HomePage.tsx
+++ src/Routes/Home/HomePage.tsx
@@ -1,14 +1,9 @@
-import { Title } from 'Components';
-import { JournalGrid } from 'Modules';
 import { PageContent } from 'Modules/PageContent';
 
 export const HomePage = () => {
   return (
     <section>
       <PageContent page="pageHome" />
-
-      <Title type="h4" title="Latest Journal Entries" />
-      <JournalGrid limit={3} />
     </section>
   );
 };
