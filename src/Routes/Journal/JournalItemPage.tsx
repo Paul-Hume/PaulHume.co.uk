7e2518c42a3b3b4f94eb35793460b3d302b00b98
Paul Hume <paul@paulhume.co.uk>
2023-02-13 11:19:47 +0100

    Dev (#37)
    * Task/cv updates (#33)
    
    * Updated style to match CV
    
    * Update home page
    
    * Refactor journal to contentful client
    
    * Update tags to use contentful client
    
    * Remove old code and files
    
    * Push selected filters to the top of the list
    
    * Add google analytics and event tracking (#35)

--- src/Routes/Journal/JournalItemPage.tsx
+++ src/Routes/Journal/JournalItemPage.tsx
@@ -1,3 +1,4 @@
+import { Helmet } from 'react-helmet-async';
 import { useParams } from 'react-router-dom';
 
 import { ErrorAlert, LoadingSpinner, NoDataAlert, RenderRichText,TagBlock,Title  } from 'Components';
@@ -5,13 +6,17 @@ import { JournalGrid } from 'Modules';
 
 import styles from './JournalItemPage.module.css';
 
+import { useUi } from 'Context/uiContext';
 import { useJournalItem  } from 'Hooks';
+import { useAnalytics } from 'Hooks/useAnalytics/useAnalytics';
 import { formatDate } from 'Utils';
 
 export const JournalItemPage = () => {
   const { slug } = useParams();
-
+  const { pageTitle } = useUi();
+  const { pageView } = useAnalytics();
   const { data, isLoading, error } = useJournalItem({ slug });
+
 
   if (isLoading) {
     return <LoadingSpinner />;
@@ -27,17 +32,24 @@ export const JournalItemPage = () => {
 
   const journalEntry = data?.items[0];
   
+  pageView();
+  
   return (
-    <section>
-      <Title title={journalEntry?.fields.title || ''} subTitle={formatDate(journalEntry?.sys.createdAt || '')} />
+    <>
+      <Helmet>
+        <title>{journalEntry?.fields?.title} {pageTitle}</title>
+      </Helmet>
+      <section>
+        <Title title={journalEntry?.fields.title || ''} subTitle={formatDate(journalEntry?.sys.createdAt || '')} />
       
-      <TagBlock align="right" size="medium" tags={journalEntry?.metadata.tags} />
+        <TagBlock align="right" size="medium" tags={journalEntry?.metadata.tags} />
 
-      { journalEntry?.fields.content && <RenderRichText className={styles.container} content={journalEntry.fields.content} />}
+        { journalEntry?.fields.content && <RenderRichText className={styles.container} content={journalEntry.fields.content} />}
 
-      <Title title="Latest Entries" type="h4" />
+        <Title title="Latest Entries" type="h4" />
 
-      <JournalGrid limit={2} journalId={journalEntry?.sys?.id} />
-    </section>
+        <JournalGrid limit={2} journalId={journalEntry?.sys?.id} />
+      </section>
+    </>
   );
 };
--- src/Routes/Journal/JournalPage.tsx
+++ src/Routes/Journal/JournalPage.tsx
@@ -1,15 +1,28 @@
+import { Helmet } from 'react-helmet-async';
 import { useOutlet } from 'react-router-dom';
 
 import { Title } from 'Components';
 import { JournalGrid } from 'Modules';
 
+import { useUi } from 'Context/uiContext';
+import { useAnalytics } from 'Hooks/useAnalytics/useAnalytics';
+
 export const JournalPage = () => {
   const outlet = useOutlet();
+  const { pageTitle } = useUi();
+  const { pageView } = useAnalytics();
+
+  pageView();
 
   return outlet || (
-    <section>
-      <Title title="Journal" />    
-      <JournalGrid />      
-    </section>
+    <>
+      <Helmet>
+        <title>Journal Page {pageTitle}</title>
+      </Helmet>
+      <section>
+        <Title title="Journal" />    
+        <JournalGrid />      
+      </section>
+    </>
   );
 };
