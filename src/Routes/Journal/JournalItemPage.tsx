a0355d697e1dd14f4ea7718ae34e6053898f92fe
Paul Hume <paul@paulhume.co.uk>
2023-02-15 15:58:09 +0100

    #10: Add project link to journal page

--- src/Routes/Journal/JournalItemPage.tsx
+++ src/Routes/Journal/JournalItemPage.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
-import { useLocation,useParams  } from 'react-router-dom';
+import { useLocation,useNavigate,useParams  } from 'react-router-dom';
+import { Button } from '@mui/material';
 
 import { ErrorAlert, LoadingSpinner, NoDataAlert, RenderRichText,TagBlock,Title  } from 'Components';
 import { JournalGrid } from 'Modules';
@@ -8,7 +9,7 @@ import { JournalGrid } from 'Modules';
 import styles from './JournalItemPage.module.css';
 
 import { useUi } from 'Context/uiContext';
-import { useJournalItem  } from 'Hooks';
+import { useJournalItem,useMedia   } from 'Hooks';
 import { useAnalytics } from 'Hooks/useAnalytics/useAnalytics';
 import { formatDate } from 'Utils';
 
@@ -18,8 +19,11 @@ export const JournalItemPage = () => {
   const { pageView } = useAnalytics();
   const { data, isLoading, error } = useJournalItem({ slug: journalSlug || slug });
   const { pathname } = useLocation();
+  const largeScreen = useMedia('md');
+  const navigate = useNavigate();
 
   const journalEntry = data?.items[0];
+  const project = journalEntry?.fields?.project?.fields;
 
   useEffect(() => {
     if (journalEntry?.fields?.title) {
@@ -48,7 +52,14 @@ export const JournalItemPage = () => {
       <section>
         <Title title={journalEntry?.fields.title || ''} subTitle={formatDate(journalEntry?.sys.createdAt || '')} />
       
-        <TagBlock align="right" size="medium" tags={journalEntry?.metadata.tags} />
+        <section className={styles['tag-container']}>
+          {project && (
+            <Button className={`${styles['project-button']} ${largeScreen ? styles['no-margin'] : ''}`} onClick={() => navigate(`/projects/${project.slug}`)}>
+                Project: {journalEntry?.fields?.project?.fields?.title}
+            </Button>
+          )}
+          <TagBlock align={largeScreen ? 'right' : 'left'} size="medium" tags={journalEntry?.metadata.tags} />
+        </section>
 
         { journalEntry?.fields.content && <RenderRichText className={styles.container} content={journalEntry.fields.content} />}
 
